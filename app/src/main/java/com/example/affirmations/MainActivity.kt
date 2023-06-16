7f8842ce6b9b961374a3d01d8727338edfb19141
SnippyRex <kentf022@gmail.com>
2023-06-17 00:52:18 +0100

    Add Affirmation List to the app display

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
@@ -45,10 +46,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationApp(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    AffirmationList(affirmationList = Datasource().loadAffirmations(),)
 }
 
 @Composable
